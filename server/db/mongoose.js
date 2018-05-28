b5a89ad1374f03f1a48c9be973c07fd55fe9b07c
andreip <andrei.popescu@infoworld.ro>
2018-05-28 20:51:52 +0300

    Solve setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; //pentru a folosi promisiuni si nu callbackuri
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose: mongoose
