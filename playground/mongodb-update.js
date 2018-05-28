48b651d1b4a95143d667fa0eb5a222f14273ee72
andreip <andrei.popescu@infoworld.ro>
2018-05-28 12:17:03 +0300

    Add update script

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -17,6 +17,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) =>{
     });//cursor, pointer catre document , toArray le introduce intr un array si reprezinta un promise
     //db.close(); 
    db.collection('Todos').findOneAndUpdate({
-       _id: 
+       _id: new ObjectID('')
+   }, {
+       $set:{
+           name: 'Vlad'
+       },
+       $inc: {
+           age: 1
+       }
+   }, {
+       returnOriginal: false
+   }).then((result) => {
+       console.log(result);
    })
 });
